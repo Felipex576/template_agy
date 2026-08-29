ce6521ce78d39353d35117fc00c9adf1b8b75dc2
Felipex576 <lfca576@hotmail.com>
2026-08-28 22:39:40 -0500

    chore: clean up scaffolded files from template repository

--- src/utils/classes.py
+++
@@ -1,8 +0,0 @@
-﻿from dataclasses import dataclass
-from pyspark.sql import DataFrame
-
-@dataclass(frozen=True)
-class ProcessingDataFrames:
-    raw_data: DataFrame
-    processed_data: DataFrame = None
-    final_summary: DataFrame = None
--- src/utils/enums.py
+++
@@ -1,8 +0,0 @@
-﻿from enum import Enum
-
-class TargetColumns(Enum):
-    ID_REGISTRO = "id_registro"
-    FECHA_CARGUE = "fecha_cargue"
-    VALOR_TOTAL = "valor_total"
-    ESTADO = "estado"
-    FECHA_PROCESO = "fecha_proceso"
